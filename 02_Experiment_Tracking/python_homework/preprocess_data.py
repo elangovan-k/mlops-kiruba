e39c8c1e49b4d15a915572428405fa520437094d
elangovan-k <66540165+elangovan-k@users.noreply.github.com>
2025-05-28 05:53:49 +0000

    "next try"

--- 02_Experiment_Tracking/python_homework/preprocess_data.py
+++ 02_Experiment_Tracking/python_homework/preprocess_data.py
@@ -14,7 +14,7 @@ def dump_pickle(obj, filename: str):
 def read_dataframe(filename: str):
     df = pd.read_parquet(filename)
 
-    df['duration'] = df['tpep_dropoff_datetime'] - df['tpep_pickup_datetime']
+    df['duration'] = df['lpep_dropoff_datetime'] - df['lpep_pickup_datetime']
     df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
     df = df[(df.duration >= 1) & (df.duration <= 60)]
 
