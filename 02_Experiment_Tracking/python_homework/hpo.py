0e080c09077008655103eb0193f0925ed4a827b6
elangovan-k <66540165+elangovan-k@users.noreply.github.com>
2025-05-29 15:42:29 +0000

    homework model updates

--- 02_Experiment_Tracking/python_homework/hpo.py
+++ 02_Experiment_Tracking/python_homework/hpo.py
@@ -1,4 +1,4 @@
-mport os
+import os
 import pickle
 import click
 import mlflow
@@ -8,7 +8,7 @@ from hyperopt.pyll import scope
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import root_mean_squared_error
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_tracking_uri("http://127.0.0.0:5000")
 mlflow.set_experiment("random-forest-hyperopt")
 
 
@@ -39,6 +39,8 @@ def run_optimization(data_path: str, num_trials: int):
         rf.fit(X_train, y_train)
         y_pred = rf.predict(X_val)
         rmse = root_mean_squared_error(y_val, y_pred)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_params(params)
 
         return {'loss': rmse, 'status': STATUS_OK}
 
--- 02_Experiment_Tracking/python_homework/train.py
+++ 02_Experiment_Tracking/python_homework/train.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import click
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import root_mean_squared_error
@@ -18,15 +19,24 @@ def load_pickle(filename: str):
     help="Location where the processed NYC taxi trip data was saved"
 )
 def run_train(data_path: str):
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")  # or use env var
+    mlflow.set_experiment("kiruba_nyc_experiment_1") 
 
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+    with mlflow.start_run():
+        mlflow.set_tag("dev_2","Kiruba")
+        
 
-    rmse = root_mean_squared_error(y_val, y_pred)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = root_mean_squared_error(y_val, y_pred)
+        min_samples_split = rf.min_samples_split
+        mlflow.log_param("min_samples_split", min_samples_split)
+       
 
 
 if __name__ == '__main__':
